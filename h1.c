304b31075a564a24cd52774b9689b9b54ebf51dd
shumaikunkun <s1711520@gmail.com>
2019-10-03 17:53:13 +0900

    発展１

--- h1.c
+++ h1.c
@@ -1,8 +1,10 @@
+//ネイピア数を求めるプログラム
 #include <stdio.h>
 
 int rec(int n){ return n==0 ? 1 : n*rec(n-1); }
 
 int main(){
-  //int max=10;
-  printf("%d\n",rec(4));
+  float ans=0;
+  for (int i=0;i<10;i++){ ans+=1.0/rec(i); }
+  printf("%f\n",ans);
 }
