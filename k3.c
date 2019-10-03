5078cf385914545635276817bf6dce2496c1b528
shumaikunkun <s1711520@gmail.com>
2019-10-03 17:34:04 +0900

    基礎3,4

---
+++ k3.c
@@ -0,0 +1,8 @@
+// sample 1-3
+#include <stdio.h>
+int main(){
+  char name[20];
+  printf("What’s your name?\n");
+  scanf("%s", name);
+  printf("Hello, %s!\n", name);
+}
---
+++ k4.c
@@ -0,0 +1,11 @@
+// sample 1-3
+#include <stdio.h>
+int main(){
+  int id1;
+  int id2;
+  printf("Puts first number\n");
+  scanf("%d", &id1);
+  printf("Puts second number\n");
+  scanf("%d", &id2);
+  printf("%d!\n", id1+id2);
+}
