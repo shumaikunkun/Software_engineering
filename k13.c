0469ab4a54beb8259a22e37566861cb36dabffff
shumaikunkun <s1711520@gmail.com>
2019-10-08 17:13:20 +0900

    課題１３、サブ関数内でポインタを入れ替えてもメイン関数で反映してくれない

---
+++ k13.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void exchange(int *x, int *y){
+  int* copy;
+
+  printf("copy:%p\n",copy);
+  printf("x:%p\n",x);
+  printf("y:%p\n",y);
+
+  copy = x;
+  printf("copy=x:%p\n",copy);
+
+  x=y;
+  printf("x=y:%p\n",x);
+
+  y=copy;
+  printf("y=copy:%p\n",y);
+}
+
+int main(){
+  int x;
+  int y;
+  printf("Input 2 numbers.\n");
+  scanf("%d",&x);
+  scanf("%d",&y);
+  printf("%d,%d\n",x,y);
+  printf("%p,%p\n",&x,&y);
+  exchange(&x,&y);
+  printf("%d,%d\n",x,y);
+  printf("%p,%p\n",&x,&y);
+}
