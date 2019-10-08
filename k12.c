f6e06d961de45c8cb19e5de84a2c4ea0db46f30f
shumaikunkun <s1711520@gmail.com>
2019-10-08 16:14:23 +0900

    課題１２のサブルーチンを再帰でワンライナー

--- k12.c
+++ k12.c
@@ -1,19 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int strLength(char *str){
-  int n = 0;
-  while( *str++ != '\0'){ n++; }
-  return n;
-}
+int strLength(char *str, int n){ return *str++ != '\0' ? strLength(str,n+1) : n; }  //文字列の要素数をポインタを用いて再帰的に求める
 
 int main(){
   char* str = (char *)malloc(sizeof(char)*10);
   char* p = str;  //先頭のポインタをコピー
   scanf("%s",str);
-
-  //printf("%d\n",strLength(str));  //引数はポインタ
-
-  for(int i=strLength(str); i>0; i--){ printf(i>1 ? "%c" : "%c\n", *(p++)); }  //一文字ずつ出力
-  for(int i=strLength(str); i>0; i--){ printf(i>1 ? "%c" : "%c\n", *(--p)); }  //逆から一文字ずつ出力
+  for(int i=strLength(str,0); i>0; i--){ printf(i>1 ? "%c" : "%c\n", *(p++)); }  //一文字ずつ出力  //最後に改行を出力するために三項演算子
+  for(int i=strLength(str,0); i>0; i--){ printf(i>1 ? "%c" : "%c\n", *(--p)); }  //逆から一文字ずつ出力
 }
