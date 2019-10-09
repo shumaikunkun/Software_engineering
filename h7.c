506f328de55002a4d16aa79132417795a6dbdd05
shumaikunkun <s1711520@gmail.com>
2019-10-10 03:42:48 +0900

    発展７、一般化

--- h7.c
+++ h7.c
@@ -6,42 +6,10 @@
 #define COL 4
 
 int main(){
-  int arr[ROW][COL];
-
   int** c = (int**)malloc(sizeof(int*)*ROW);
-
-
-  *c = (int*)malloc(sizeof(int)*COL);
-  *(c+1) = (int*)malloc(sizeof(int)*COL);
-  *(c+2) = (int*)malloc(sizeof(int)*COL);
-
-
-
-  // printf("%p\n",arr);  //arr[0] p
-  // printf("%p\n",arr+1);
-  // printf("%p\n",arr+2);
-  // printf("\n");
-  // printf("%p\n",*arr);  //arr[0][0] p
-  // printf("%p\n",*arr+1);  //arr[0][1] p
-  // printf("%p\n",*arr+2);
-  // printf("%p\n",*arr+3);
-  // printf("%p\n",*(arr+1));
-  // printf("%p\n",*(arr+1)+1);
-  // printf("%p\n",*(arr+2)+2);
-  // printf("%p\n",*(arr+2)+3);
-  // printf("\n");
-  // printf("%d\n",**arr);
-  // printf("%d\n",*(*(arr+2)+2)); //arr[2][2]
-
-  for(int i=0; i<ROW; i++){
-    for(int j=0; j<COL; j++){
-      *(*(c+i)+j)=10;
-    }
-  }
-
-  for(int i=0; i<ROW; i++){
-    for(int j=0; j<COL; j++){
-      printf("%d\n",*(*(c+i)+j));
-    }
-  }
+  for(int i=0; i<ROW; i++){ *(c+i) = (int*)malloc(sizeof(int)*COL); }
+  //全要素に10を代入
+  for(int i=0; i<ROW; i++){ for(int j=0; j<COL; j++){ *(*(c+i)+j)=10; }}
+  //全要素の表示
+  for(int i=0; i<ROW; i++){ for(int j=0; j<COL; j++){ printf("%d\n",*(*(c+i)+j)); }}
 }
