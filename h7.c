d6a51beb213d326868862679f86ca0e5483cbaaf
shumaikunkun <s1711520@gmail.com>
2019-10-10 03:45:42 +0900

    ファイル追加

--- h7.c
+++ h7.c
@@ -8,8 +8,6 @@
 int main(){
   int** c = (int**)malloc(sizeof(int*)*ROW);
   for(int i=0; i<ROW; i++){ *(c+i) = (int*)malloc(sizeof(int)*COL); }
-  //全要素に10を代入
-  for(int i=0; i<ROW; i++){ for(int j=0; j<COL; j++){ *(*(c+i)+j)=10; }}
-  //全要素の表示
-  for(int i=0; i<ROW; i++){ for(int j=0; j<COL; j++){ printf("%d\n",*(*(c+i)+j)); }}
+  for(int i=0; i<ROW; i++){ for(int j=0; j<COL; j++){ *(*(c+i)+j)=10; }}  //全要素に10を代入
+  for(int i=0; i<ROW; i++){ for(int j=0; j<COL; j++){ printf("%d\n",*(*(c+i)+j)); }}  //全要素の表示
 }
