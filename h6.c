f9bfd852e0f7383e9edf9cc9d5837a12c83622b1
shumaikunkun <s1711520@gmail.com>
2019-10-10 04:04:28 +0900

    課題６、完成

--- h6.c
+++ h6.c
@@ -2,35 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//int strLength(char *str, int n){ return *str++ != '\0' ? strLength(str,n+1) : n; }  //文字列の要素数をポインタを用いて再帰的に求める
-
 void myscanf(char* str){
-  //char* p = str;  //先頭ポインタのコピー
   char c;
-  char* p;
-  for(int i=0; i<5; i++){
-    //c=getc(stdin);
-    printf("%p\n",&c);
-    printf("%c\n",c);
-    //str=c;
-    (&c)++;
-
-    // *p=c;
-    // printf("%p\n",p);
-    // printf("%c\n",*p);
-
-    //str=p
-
-    //p++;
-
-
-
-    //printf("%p\n",str++);
-    //printf("%c\n",*str);
+  for(int i=0; i<10; i++){
+    c=getc(stdin);
+    if(c=='\n') break;
+    *(str+i)=c;
   }
-
   printf("-------%s\n",str);
-  //for(int i=0; i<strLength(str,0); i++) printf("%c",*p++);
 }
 
 int main(){
