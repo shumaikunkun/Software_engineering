faab83de38a23e166da3875107a688eff4ce128f
shumaikunkun <s1711520@gmail.com>
2019-10-10 04:11:13 +0900

    発展６、７、コメントアウト

--- h6.c
+++ h6.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void myscanf(char* str){ for(int i=0; i<100; i++){ if((*(str+i)=getc(stdin))=='\n') break; }}
+void myscanf(char* str){ for(int i=0; i<100; i++){ if((*(str+i)=getc(stdin))=='\n') break; }}  //100文字まで格納できる
 
 int main(){
   char* str = (char *)malloc(sizeof(char)*5);
