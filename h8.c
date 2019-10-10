72ccc94e84c8351e9fbd551bcc00699d1ff771d1
shumaikunkun <s1711520@gmail.com>
2019-10-10 13:57:14 +0900

    発展８、完了（冗長なのでリファクタリングの余地あり）

--- h8.c
+++ h8.c
@@ -1,4 +1,4 @@
-//構造体の再帰的な参照によってリストを実装
+//構造体の再帰的な参照によって実装されたリストに新たな要素を挿入
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -14,10 +14,28 @@ int main(){
   head = a;  //先頭ポインタをメモ
   FILE *fp;
   fp = fopen("company.txt", "r");
+  FILE *fp_add;
+  fp_add = fopen("add.txt", "r");  //追加用のデータファイル
 
+  int n;
+  printf("何番目にデータを挿入しますか？\n");
+  scanf("%d",&n);  //追加するインデックスを入力
+
+  int i=0;
+  if(n==0){  //0番目だった時の挿入処理
+    fscanf(fp_add, "%s %s", a->name, a->email);
+    a->next = (node*)malloc(sizeof(node));  //次の領域を確保
+    a = a->next;  //次の領域に移動
+  }
   while(fscanf(fp, "%s %s", a->name, a->email)!=EOF){
     a->next = (node*)malloc(sizeof(node));  //次の領域を確保
     a = a->next;  //次の領域に移動
+    i++;
+    if(i==n){  //挿入処理
+      fscanf(fp_add, "%s %s", a->name, a->email);
+      a->next = (node*)malloc(sizeof(node));  //次の領域を確保
+      a = a->next;  //次の領域に移動
+    }
   }
 
   a = head;  //先頭に戻る
