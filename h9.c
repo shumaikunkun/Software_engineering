93aad77b69ed9b010d11dbeb60c46bceaad24600
shumaikunkun <s1711520@gmail.com>
2019-10-10 22:40:46 +0900

    課題９、完了

--- h9.c
+++ h9.c
@@ -1,4 +1,4 @@
-//構造体の再帰的な参照によって木構造を実装         ./a.out  < company.txt
+//構造体の再帰的なメンバ参照によって木構造を実装
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -11,7 +11,6 @@ typedef struct node {
 node* sub(FILE* fp, node* a, int n){
   a=(node*)malloc(sizeof(node));
   fscanf(fp, "%s", a->name);
-  printf("%s\n",a->name);
   if(n==0){  //葉まで達したらnodeメンバをNULLにして抜ける
     a->next1=NULL;
     a->next2=NULL;
@@ -19,23 +18,21 @@ node* sub(FILE* fp, node* a, int n){
     a->next1=sub(fp, a->next1, n-1);
     a->next2=sub(fp, a->next2, n-1);
   }
-  printf("----%s\n", a->name);
   return a;  //ポインタを返す
 }
 
 int main(){
-  node *a, *head;
-  a = (node*)malloc(sizeof(node));
-  head = a;  //先頭ポインタをメモ
-
   FILE *fp;
   fp = fopen("tree.txt", "r");
+  node *a;
+  a = sub(fp, a, 2);  //深さ２までの木構造, ポインタを戻す
 
-  sub(fp, a, 2);  //深さ２までの木構造
-  a = head;  //ポインタを先頭に戻す
-
-  printf("%s\n", a->name);
-  printf("%s\n", a->next1->name);
-  printf("%s\n", a->next1->next1->name);
+  printf("1.%s\n", a->name);
+  printf("2.%s\n", a->next1->name);
+  printf("3.%s\n", a->next1->next1->name);
+  printf("4.%s\n", a->next1->next2->name);
+  printf("5.%s\n", a->next2->name);
+  printf("6.%s\n", a->next2->next1->name);
+  printf("7.%s\n", a->next2->next2->name);
 
 }
