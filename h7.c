6a16e765fb91d86e7bbd612cc14c60c0c6f44ae4
shumaikunkun <s1711520@gmail.com>
2019-10-14 20:19:28 +0900

    発展７、リファクタリング

--- h7.c
+++ h7.c
@@ -7,7 +7,9 @@
 
 int main(){
   int** c = (int**)malloc(sizeof(int*)*ROW);  //二次元配列の要素の領域確保
-  for(int i=0; i<ROW; i++){ *(c+i) = (int*)malloc(sizeof(int)*COL); }  //1次元配列それぞれの領域確保
-  for(int i=0; i<ROW; i++){ for(int j=0; j<COL; j++){ *(*(c+i)+j)=10; }}  //全要素に10を代入
+  for(int i=0; i<ROW; i++){
+    *(c+i) = (int*)malloc(sizeof(int)*COL);  //1次元配列それぞれの領域確保
+    for(int j=0; j<COL; j++){ *(*(c+i)+j) = i+j; }  //全要素に行番号＋列番号を代入
+  }
   for(int i=0; i<ROW; i++){ for(int j=0; j<COL; j++){ printf("%d\n",*(*(c+i)+j)); }}  //全要素の表示
 }
